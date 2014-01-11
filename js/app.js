daa2f8e5192ab1a4356228ae9c735f90b9dbc9f8
Tyler McGinnis <tylermcginnis33@gmail.com>
2014-01-10 23:35:05 -0700

    Finish app

--- js/app.js
+++ js/app.js
@@ -1,7 +1,1 @@
 angular.module('myApp', ['ngResource','ngRoute', 'ngGrid', 'myApp.controllers', 'myApp.services'])
-  .config(['$routeProvider', function($routeProvider){
-    // $routeProvider
-    //   .when('/', {
-    //     controller: 'mainCtrl',
-    //   });
-  }]);
--- js/controllers.js
+++ js/controllers.js
@@ -37,7 +37,7 @@ angular.module('myApp.controllers', [])
         searchInfo[i].trackName && (correctData['Song'] = searchInfo[i].trackName);
         searchInfo[i].artistName && (correctData['Artist'] = searchInfo[i].artistName);
         searchInfo[i].collectionName && (correctData['Collection'] = searchInfo[i].collectionName);
-        searchInfo[i].artworkUrl60 && (correctData['AlbumArt'] = searchInfo[i].artworkUrl60);
+        searchInfo[i].artworkUrl60 && (correctData['AlbumArt'] = searchInfo[i].artworkUrl100);
         searchInfo[i].kind && (correctData['Type'] = searchInfo[i].kind);
         searchInfo[i].trackPrice && (correctData['IndividualPrice'] = searchInfo[i].trackPrice);
         searchInfo[i].collectionPrice && (correctData['CollectionPrice'] = searchInfo[i].collectionPrice);
@@ -47,8 +47,19 @@ angular.module('myApp.controllers', [])
 
     $scope.gridOptions = { 
       data: 'theData',
+      height: '110px',
       filterOptions: $scope.filterOptions,
-      sortInfo: {fields: ['Song', 'Artist', 'Collection', 'Type'], directions: ['asc']}
+      sortInfo: {fields: ['Song', 'Artist', 'Collection', 'Type'], directions: ['asc']},
+      columnDefs: [
+        {field: 'Play', displayName: 'Play', width: '40px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><a href="{{row.getProperty(col.field)}}"><img src="http://www.icty.org/x/image/Miscellaneous/play_icon30x30.png"></a></div>'},
+        {field: 'Song', displayName: 'Song'},
+        {field: 'Artist', displayName: 'Artist'},
+        {field: 'Collection', displayName: 'Collection'},
+        {field: 'AlbumArt', displayName: 'Album Art', width: '110px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><img src="{{row.getProperty(col.field)}}"></div>'},
+        {field: 'Type', displayName: 'Type'},
+        {field: 'IndividualPrice', displayName: 'Single Price'},
+        {field: 'CollectionPrice', displayName: 'Collection Price'},
+      ]
     };
 
   }]);
